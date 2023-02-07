068008195c97b24d2bc4bd3acf543ef42f803a1c
Luiz Claudio <luiz.deadzone@gmail.com>
2023-02-07 10:11:58 -0300

    regex nome aceitando pontos

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,4 +1,4 @@
-export const REGEX_NAME = '^[A-zÀ-ú ]+$';
+export const REGEX_NAME = '^[A-zÀ-ú. ]+$';
 export const REGEX_FULL_NAME =
   /^([A-zÀ-ú.]{3,})([A-zÀ-ú.\s]*)\s([A-zÀ-ú.]{3,})/;
 export const REGEX_NUMBER = '^[0-9]+$';
