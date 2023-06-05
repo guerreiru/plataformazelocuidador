1d6cf79f116367f3ed97c69afc46f79fede08da2
Luiz Claudio <luiz.deadzone@gmail.com>
2023-06-05 12:42:30 -0300

    [ PDF ]: Modal de senha nos relatórios
    Modal customizado para senhas nos Relatórios em PDF

--- src/components/Modal/ModalViewPDF/index.js
+++ src/components/Modal/ModalViewPDF/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import PDFReader from 'rn-pdf-reader-js';
-import { Modal, View, TouchableOpacity, TextClose } from './styles';
+import React, { useState } from 'react';
+import PDFReader from 'rn-pdf-reader-js-with-password';
+// import PDFReader from 'rn-pdf-reader-js';
+import { View, TouchableOpacity, TextClose, Footer, TextButton, PassTextInput, Modal as ReactModal } from './styles';
+import Modal from '..';
+
+import { useSelector } from 'react-redux';
+import { Button } from '../styles';
+import colors from '../../../styles/colors';
 
 const ModalViewPDF = ({
   visible,
@@ -11,20 +17,58 @@ const ModalViewPDF = ({
     return false;
   },
 }) => {
+  const { user } = useSelector((state) => state.auth);
+  const [password, setPassword] = useState('');
+  const [isCorrect, setIsCorrect] = useState(undefined);
+
+  const handleSubmit = () => {
+    if (user.cpf.replace('.', '').slice(0, 6) === password) {
+      setIsCorrect(true);
+    } else {
+      setIsCorrect(false);
+    }
+  }
+
+  if (isCorrect !== true) {
+    return (
+      <Modal
+        visible={true}
+        title='Senha do PDF:'
+        message={isCorrect === undefined ? 'Insira a senha para visualizar o PDF' : 'Senha incorreta, tente novamente.'}
+        footer={
+          <Footer>
+            <Button onPress={() => onCancel()}><TextButton color={colors.ERROR}>Cancelar</TextButton></Button>
+            <Button onPress={() => handleSubmit()}><TextButton>Ok</TextButton></Button>
+          </Footer>
+        }
+      >
+        <PassTextInput
+          onBlur={() => handleSubmit()}
+          keyboardType='numeric'
+          onChangeText={(txt) => { setPassword(txt);}}
+          blurOnSubmit
+        />
+      </Modal>
+    );
+  }
+
   return (
-    <Modal animationType="fade" transparent={false} visible={visible}>
+    <ReactModal animationType="fade" transparent={false} visible={visible}>
       <View>
         <TouchableOpacity onPress={onCancel}>
           <TextClose> FECHAR</TextClose>
         </TouchableOpacity>
         <PDFReader
-          source={{ base64: 'data:application/pdf;base64,' + base64 }}
+          source={{
+            base64: 'data:application/pdf;base64,' + base64,
+          }}
+          password={password}
         />
         <TouchableOpacity onPress={() => saveFile(base64)}>
           <TextClose> SALVAR/COMPARTILHAR</TextClose>
         </TouchableOpacity>
       </View>
-    </Modal>
+    </ReactModal>
   );
 };
 
--- src/components/Modal/ModalViewPDF/styles.js
+++ src/components/Modal/ModalViewPDF/styles.js
@@ -1,6 +1,7 @@
 import { SafeAreaView } from 'react-native';
 import styled from 'styled-components/native';
 import colors from '../../../styles/colors';
+import { TextInput } from 'react-native';
 
 export const Modal = styled.Modal``;
 
@@ -20,4 +21,24 @@ export const TouchableOpacity = styled.TouchableOpacity`
 export const TextClose = styled.Text`
   font-size: 16px;
   color: ${colors.WHITE};
+`;
+
+export const PassTextInput = styled(TextInput)`
+  margin: auto;
+  margin-top: 16px;
+  width: 100%;
+  border: 1px solid #aaa;
+  padding: 0 8px;
+`;
+
+export const TextButton = styled.Text`
+  font-size: 14px;
+  font-weight: 700;
+  color: ${({ color }) => color ? color : colors.PRIMARY};
+`;
+
+export const Footer = styled.View`
+  flex-direction: ${({ direction }) => direction || 'row'};
+  padding: 0px 8px 8px 24px;
+  justify-content: flex-end;
 `;
